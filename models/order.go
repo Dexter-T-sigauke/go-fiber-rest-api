b160ad58081b316a8b3e42c7e0dc6670224e15d7
Dexter-T-sigauke <tafadzwasigauke123@gmail.com>
2024-07-01 16:38:01 +0200

    added uuid to product referer

--- models/order.go
+++ models/order.go
@@ -1,12 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Order struct {
 	ID           uint `json:"id" gorm:"primaryKey"`
 	CreatedAt    time.Time
 	ProductRefer int     `json:"product_id"`
 	Product      Product `gorm:"foreignKey:ProductRefer"`
-	UserRefer    int     `json:"user_id"`
+	UserRefer    uuid.UUID     `json:"user_id"`
 	User         User    `gorm:"foreignKey:UserRefer"`
 }
