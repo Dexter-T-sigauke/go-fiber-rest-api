8b42e906343866d95df9ee3068d15268bba741c6
Dexter-T-sigauke <tafadzwasigauke123@gmail.com>
2024-07-01 16:30:48 +0200

    Added uuid to user model

--- models/user.go
+++ models/user.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+	"github.com/google/uuid"
+
+)
 
 type User struct {
-	ID        uint `json:"id" gorm:"primaryKey"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	CreatedAt time.Time
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
